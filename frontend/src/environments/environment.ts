7ab587c337721dcd3e6c98831a47f5f61ee74e89
Cole Merkosky <cmerkdev@gmail.com>
2026-03-28 19:05:10 -0600

    Amend finalized production URL

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://weather-backend-h8bgdag0azfvh8f9.canadacentral-01.azurewebsites.net'
+    baseUrl: 'https://weather-backend-h8bgdag0azfvh8f9.canadacentral-01.azurewebsites.net/api'
 };
